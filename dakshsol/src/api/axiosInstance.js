42054889507569c140db1d7273736ac2827a0c27
soham3131 <sohamdang0@gmail.com>
2025-10-26 21:38:55 +0530

    chnged react url

--- dakshsol/src/api/axiosInstance.js
+++ dakshsol/src/api/axiosInstance.js
@@ -1,25 +1,14 @@
-// import axios from 'axios';
-
-// // Get the backend URL from the environment variable.
-// // We use a fallback (e.g., 'http://localhost:5000/api') for development when the variable might not be set.
-
-// const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api';
-
-// export default axios.create({
-//   // Use the dynamically set URL
-//   baseURL: API_BASE_URL, 
-//   headers: { 'Content-Type': 'application/json' },
-//   withCredentials: true
-// });
-
-
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL;
 
-const axiosInstance = axios.create({
-  baseURL,
-  withCredentials: true,
+
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api';
+
+export default axios.create({
+  // Use the dynamically set URL
+  baseURL: API_BASE_URL, 
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true
 });
 
-export default axiosInstance;
+
